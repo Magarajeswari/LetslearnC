a7cb632e2ef74675d349d7e4624c7da0e9bc9ac5
Magarajeswari <52660002+Magarajeswari@users.noreply.github.com>
2023-06-17 09:17:47 +0530

    Add files via upload

---
+++ area of a rectangle.c
@@ -0,0 +1,14 @@
+/*Program to find the area of a rectangele*/
+/*Author : Magarajeswari 12-06-2023*/
+#include<stdio.h>
+int main()
+{
+int length;
+int breadth;
+int area;
+length=10;
+breadth=14;
+area=length*breadth;
+printf("Area of the rectangle is %d\n",area);
+return 0;
+}
